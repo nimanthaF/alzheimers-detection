a91f5f6914526cc4315cbbee35293dc977ba1b9c
nimanthaF <nimanthafernando95@gmail.com>
2021-04-01 05:25:40 -0400

    rendering templates

--- trained model/predict.py
+++ trained model/predict.py
@@ -1,30 +1,37 @@
 import cv2
 import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 from keras.models import load_model
 import matplotlib.pyplot as plt
+from keras.preprocessing import image
 
 model=load_model('alzheimers.h5')
 
+img_path="test/NonDemented/26.jpg"
 img_pred=cv2.imread("test/NonDemented/26.jpg")
 
-predicted_data=np.expand_dims(img_pred,axis=0)
-prediction=model.predict(predicted_data)
+def model_prediction(img_path,model):
+    img=image.load_img(img_path,target_size=(208,176))
+    predicted_data=np.expand_dims(img,axis=0)
+    prediction=model.predict(predicted_data)
 
-i=0
+    i=0
 
-for i in range(4):
-    if prediction[0][i]==1.00:
-        if i==0:
-            print('Mild Demented')
-        elif i==1:
-            print('Modearate Demented')
-        elif i==2:
-            print('No Alzheimer')
-        else:
-            print('Very Mild Demented')
+    for i in range(4):
+        if prediction[0][i]==1.00:
+            if i==0:
+                print('Mild Demented')
+            elif i==1:
+                print('Modearate Demented')
+            elif i==2:
+                print('No Alzheimer')
+            else:
+                print('Very Mild Demented')
 
-    i+=1
+        i+=1
+
+model_prediction(img_path,model)
 
 plt.imshow(img_pred)
 plt.show()
